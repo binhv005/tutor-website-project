cd110f734f3146e7476fa1547a5b07b458689328
binhv005 <vtb22522005@gmail.com>
2026-07-04 08:04:24 +0700

    refactor: optimize ContactCard component structure

--- tutor-frontend/src/components/ContactCard.jsx
+++ tutor-frontend/src/components/ContactCard.jsx
@@ -1,25 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCheck,
-  faShieldHalved,
-  faHeadset,
-} from "@fortawesome/free-solid-svg-icons";
 
-function ContactCard() {
-  const commit = [
-    {
-      icon: faShieldHalved,
-      text: "Bảo mật thông tin 100%",
-    },
-    {
-      icon: faHeadset,
-      text: "Hỗ trợ 24/24",
-    },
-    {
-      icon: faCheck,
-      text: "Hơn 15 năm uy tín",
-    },
-  ];
+function ContactCard({ data }) {
   return (
     <div className="px-5 py-20 bg-tertiary ">
       <div className="text-center text-white">
@@ -35,17 +16,18 @@ function ContactCard() {
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 py-5">
         <input
           type="text"
+          name="phoneInput"
           placeholder="Số điện thoại của bạn"
           className="bg-white px-3 py-5 rounded-xl"
         />
-        <button className="bg-primary text-white rounded-xl px-3 py-5 hover:bg-red-600`">
+        <button className="bg-primary text-white rounded-xl px-3 py-5 hover:bg-red-600">
           Đăng Ký Tư Vấn Ngay
         </button>
       </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-3 lg:ml-50 gap-5">
-        {commit.map((item, index) => (
-          <div key={index} className="flex items-center gap-3">
+      <div className="grid grid-cols-1 lg:grid-cols-3 lg:max-w-screen-md lg:mx-auto gap-5">
+        {data.map((item) => (
+          <div key={item.id} className="flex items-center gap-3">
             <FontAwesomeIcon icon={item.icon} className="text-secondary" />
             <p className="text-white">{item.text}</p>
           </div>
