38854ce42d8798eb1f2870b247aa352e4b4e8a2d
binhv005 <vtb22522005@gmail.com>
2026-07-04 08:04:20 +0700

    feat: add ScrollToTop component and update App routes for automatic scroll reset

--- tutor-frontend/src/App.jsx
+++ tutor-frontend/src/App.jsx
@@ -2,9 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PublicLayout from "./layouts/PublicLayout";
 import Home from "./pages/Home";
 import TuitionPage from "./pages/TuitionPage";
+import ScrollToTop from "./components/ScrollToTop";
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<PublicLayout />}>
           <Route index element={<Home />} />
