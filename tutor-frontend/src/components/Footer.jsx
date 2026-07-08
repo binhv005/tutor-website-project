b883a13746ac87fe8bd968568f8c15c94f408317
binhv005 <vtb22522005@gmail.com>
2026-07-08 17:15:52 +0700

    refactor(footer): remove logo and support column

--- tutor-frontend/src/components/Footer.jsx
+++ tutor-frontend/src/components/Footer.jsx
@@ -4,12 +4,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPhone,
   faEnvelope,
-  faHeadset,
   faLocationDot,
 } from "@fortawesome/free-solid-svg-icons";
 
 function Footer() {
-  const menuIcon = [faPhone, faEnvelope, faHeadset];
   const serviceMenu = [
     "Gia sư Tiểu Học",
     "Gia sư THCS",
@@ -17,12 +15,6 @@ function Footer() {
     "Luyện thi vào 10",
     "Luyện thi Đại Học",
     "Luyện thi IELTS",
-  ];
-
-  const supportMenu = [
-    "Về chúng tôi",
-    "Điều khoản dịch vụ",
-    "Chính sách quyền riêng tư",
   ];
 
   const contactMenu = [
@@ -42,7 +34,7 @@ function Footer() {
 
   return (
     <footer className="bg-neutral px-5 py-10">
-      <div className="grid grid-cols-1 lg:grid-cols-4 gap-6">
+      <div className="grid grid-cols-1 lg:grid-cols-3 lg: place-items-center">
         <div>
           <div className="flex items-center">
             <img src={tutorLogo} alt="logo" className="w-12 py-3" />
@@ -52,15 +44,6 @@ function Footer() {
             Trung tâm gia sư hàng đầu Việt Nam, chuyên cung cấp các giải pháp
             học tập cá nhân hóa cho học sinh mọi lứa tuổi.
           </p>
-          <ul className="flex gap-8 py-3">
-            {menuIcon.map((item, index) => (
-              <li key={index}>
-                <button className="text-primary bg-white p-2 rounded-lg transition-all duration-300 hover:bg-primary hover:text-white">
-                  <FontAwesomeIcon icon={item} />
-                </button>
-              </li>
-            ))}
-          </ul>
         </div>
 
         <div>
@@ -69,17 +52,6 @@ function Footer() {
             {serviceMenu.map((item, index) => (
               <li key={index} className="py-1 text-headline hover:text-primary">
                 <Link to="/services">{item}</Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <div>
-          <h1 className="font-bold py-4 text-2xl">HỖ TRỢ</h1>
-          <ul>
-            {supportMenu.map((item, index) => (
-              <li key={index} className="py-1 text-headline hover:text-primary">
-                <Link to="/supports">{item}</Link>
               </li>
             ))}
           </ul>
